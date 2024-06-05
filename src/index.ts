ff7e2cd91fa1b1e0d34af48f342d4564683283e2
SeongKookKIM <ksk8646@naver.com>
2024-06-05 20:45:38 +0900

    Fix/Add vercel.json, 배포 세팅

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import quizData from "../data/data";
-import { error } from "console";
 
 const app = express();
 
@@ -14,7 +13,7 @@ app.listen(8080, () => {
   console.log("https://b1bd-220-121-186-219.ngrok-free.app/");
 });
 
-app.get("/welcome", (req: Request, res: Response) => {
+app.get("/socrates", (req: Request, res: Response) => {
   console.log(quizData);
 
   return res.status(200).json(quizData);
